6aed63d3226f8ca37780dc927419adabac299629
Aksaule <aksaule124026@gmail.com>
2025-04-01 10:45:09 +0500

    Day 2, leetcode problem solving

--- python_section/leetcode_tasks.py
+++ python_section/leetcode_tasks.py
@@ -5,23 +5,56 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        new_listnode = ListNode()
-        current = new_listnode
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         new_listnode = ListNode()
+#         current = new_listnode
+#
+#         while list1 and list2:
+#             if list1.val >= list2.val:
+#                 current.next = ListNode(list2.val)
+#                 list2 = list2.next
+#             else:
+#                 current.next = ListNode(list1.val)
+#                 list1 = list1.next
+#             current = current.next
+#
+#         if list1:
+#             current.next = list1
+#         else:
+#             current.next = list2
+#
+#         return new_listnode.next
 
-        while list1 and list2:
-            if list1.val >= list2.val:
-                current.next = ListNode(list2.val)
-                list2 = list2.next
-            else:
-                current.next = ListNode(list1.val)
-                list1 = list1.next
-            current = current.next
 
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
+######35 Search Insert Position
+####my solution
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         try:
+#             return nums.index(target)
+#
+#         except:
+#             nums.append(target)
+#             print(nums)
+#             nums.sort()
+#             return nums.index(target)
 
-        return new_listnode.next
+######chatgpt solution
+# nums = [1,3,5,6]
+# target = 5
+#
+# left, right = 0, len(nums)-1
+# while left <= right:
+#     mid = (left+right)//2
+#
+#     if nums[mid] == target:
+#         print(mid)
+#
+#     elif nums[mid] > target:
+#         right-=1
+#
+#     else:
+#         left+=1
+
+
